bf8527d846e9617518d2590ea73ee0f842804892
Jbricio <108837758+Jbricio@users.noreply.github.com>
2022-07-27 12:49:42 -0500

    Palindrome method added

---
+++ MethodsColections.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgramacionIntermedia2
+{
+    public static class MethodsColections
+    {
+        public static void ReverseWithQueue(Queue<int> queque)
+        {
+            Stack<int> stack = new Stack<int>();
+            while(queque.Count > 0)
+            {
+                stack.Push(queque.Dequeue());
+            }
+            while(stack.Count > 0)
+            {
+                queque.Enqueue(stack.Pop());
+            }
+            do
+            {
+                Console.WriteLine(queque.Dequeue());
+            } while (queque.Count > 0);
+        }
+
+        public static void ReverseWithList(List<int> list)
+        {
+            list.Reverse();
+            foreach(var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public static void ReverseWithDictionary(Dictionary<int,string> dic)
+        {
+            Dictionary<int,string> resultDic = new Dictionary<int,string>();
+            foreach(var item in dic.Reverse())
+            {
+                resultDic[item.Key] = item.Value;
+                Console.WriteLine(item.Key.ToString(),item.Value);
+            }
+            
+        }
+        public static bool EvaluateIfIsPalindrome(string word)
+        {
+            string half1 = word.Substring(0, word.Length / 2);
+            char[] arr = word.ToCharArray();
+            Array.Reverse(arr);
+            string copy = new string(arr);
+            string half2 = copy.Substring(0, copy.Length / 2);
+
+            return half1.Equals(half2);
+        }
+    }
+}
