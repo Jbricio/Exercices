771b7e21e5536d623db9585a5d9364118b1fc7f2
Jbricio <108837758+Jbricio@users.noreply.github.com>
2022-07-29 11:27:10 -0500

    Advance programming practice 1 added

---
+++ ProramacionAvanzada/ProramacionAvanzada/ExampleDegelates.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProramacionAvanzada
+{
+    public delegate void EvenNumberDelegate(List<int> numbers);
+    public delegate void OddNumberDelegate(List<int> numbers);
+    public delegate void OrderByAscendingDelegate(List<int> numbers);
+    public delegate void OrderByDescendingDelegate(List<int> numbers);
+    public delegate void EvaluatesMultipleOf3Delegate(int num);
+    public delegate void SendTextDelegate(string txt);
+    public delegate void GetMaximunValueDelegate(List<int> numbers);
+    public delegate void GetMinimunValueDelegate(List<int> numbers);
+    public delegate void AverageDelegate(List<int> numbers);
+    public delegate void GetRMSDelegate(List<int> numbers);
+
+    internal static class ExampleDegelates
+    {
+        public static void EvenNumberDelegateMethod(List<int> numbers)
+        {
+            List<int> numbers2 = new List<int>();
+            foreach(int n in numbers)
+            {
+                if (n % 2==0)
+                {
+                    numbers2.Add(n);
+                }
+            }
+            foreach(int n in numbers2)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        
+        public static void OddNumberDelegateMethod(List<int> numbers)
+        {
+            List<int> numbers2 = new List<int>();
+            foreach (int n in numbers)
+            {
+                if (n % 2 != 0)
+                {
+                    numbers2.Add(n);
+                }
+            }
+            foreach (int n in numbers2)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        
+        public static void OrderByAscendingMethod(List<int> list1)
+        {
+            list1.Sort();
+            foreach(var n in list1)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        
+        public static void OrderByDescendingMethod(List<int> list1)
+        {
+            list1.Reverse();
+            foreach (var n in list1)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        public static void FindMultipleOf3Method(int num)
+        {
+            if (num % 3 == 0)
+                Console.WriteLine(num + " is multiple of the number 3");
+            else
+                Console.WriteLine(num + " is not multiple of 3");
+        }
+
+        public static void SendTextMethod(string txt)
+        {
+            Console.WriteLine("Text passed: " +txt);
+        }
+
+        public static void GetMaximunValueMethod(List<int> numbers)
+        {
+            Console.WriteLine(numbers.Max()+" is the maximun value of the list given.");
+        }
+
+        public static void GetMinimunValueMethod(List<int> numbers)
+        {
+            Console.WriteLine(numbers.Min() + " is the minimun value of the list given.");
+        }
+
+        public static void AverageDelegateMethod(List<int> avList)
+        {
+            Console.WriteLine("Average: "+avList.AsQueryable().Average());
+        }
+
+        public static void GetRMSMethod(List<int> numbers)
+        {
+            var cant= numbers.Count();
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                numbers[i] = numbers[i] * numbers[i];
+            }
+            int sum = numbers.Aggregate((x, y) => x + y);
+            float res = sum / cant;
+            Console.WriteLine("The result is: "+Math.Sqrt(res));
+        }
+    }
+}
---
+++ ProramacionAvanzada/ProramacionAvanzada/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProramacionAvanzada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> enterosList = new List<int>();
+            for (int i = 0; i < 20; i++)
+            {
+                enterosList.Add(i);
+            }
+            EvenNumberDelegate evenNumberDelegate = ExampleDegelates.EvenNumberDelegateMethod;
+            evenNumberDelegate(enterosList);
+            //
+            Console.WriteLine("=================================\nOdd numbers:");
+            List<int> oddList = new List<int>();
+            for (int i = 0; i < 20; i++)
+            {
+                oddList.Add(i);
+            }
+            OddNumberDelegate oddNumberDelegate = ExampleDegelates.OddNumberDelegateMethod;
+            oddNumberDelegate(oddList);
+            //
+            Console.WriteLine("=================================\nOrder by ascending:");
+            OrderByAscendingDelegate orderByAscendingDelegate = ExampleDegelates.OrderByAscendingMethod;
+            List<int> ascList = new List<int>();
+            for (int i = 20; i > 0; i--)
+            {
+                ascList.Add(i);
+            }
+            orderByAscendingDelegate(ascList);
+            //
+            Console.WriteLine("=================================\nOrder by Descending:");
+            OrderByDescendingDelegate orderByDescendingDelegate = ExampleDegelates.OrderByDescendingMethod;
+            List<int> descList = new List<int>();
+            for (int i = 0; i <=20; i++)
+            {
+                descList.Add(i);
+            }
+            orderByDescendingDelegate(descList);
+            Console.WriteLine("=================================\nEvaluate multiple of 3:");
+            EvaluatesMultipleOf3Delegate evaluatesMultipleOf3 = ExampleDegelates.FindMultipleOf3Method;
+            evaluatesMultipleOf3(33);
+            Console.WriteLine("=================================\nSend a text:");
+            SendTextDelegate sendTextDelegate = ExampleDegelates.SendTextMethod;
+            sendTextDelegate("This is the sended text");
+            Console.WriteLine("=================================\nFind maximun value:");
+            GetMaximunValueDelegate getMaximunVallueDelegate = ExampleDegelates.GetMaximunValueMethod;
+            List<int> maxList = new List<int>();
+            for (int i = 0; i <= 20; i++)
+            {
+                maxList.Add(i);
+            }
+            getMaximunVallueDelegate(maxList);
+            Console.WriteLine("=================================\nFind minimun value:");
+            GetMinimunValueDelegate getMinimunVallueDelegate = ExampleDegelates.GetMinimunValueMethod;
+            List<int> minList = new List<int>();
+            for (int i = 0; i <= 20; i++)
+            {
+                minList.Add(i);
+            }
+            getMinimunVallueDelegate(minList);
+            Console.WriteLine("=================================\nCalculate average:");
+            AverageDelegate averageDelegate = ExampleDegelates.AverageDelegateMethod;
+            Random rd = new Random();
+            List<int> averageList = new List<int>();
+            for(int i = 0; i <= 10; i++)
+            {
+                averageList.Add(rd.Next(1, 50));
+            }
+            averageDelegate(averageList);
+            Console.WriteLine("=================================\nCalculate RMS:");
+            GetRMSDelegate getRMSDelegate = ExampleDegelates.GetRMSMethod;
+            List<int> rmsList = new List<int>();
+            for (int i = 0; i <= 10; i++)
+            {
+                rmsList.Add(rd.Next(1, 50));
+            }
+            getRMSDelegate(rmsList);
+            //action, funct and predicate
+
+            //Func
+            Func<double, double> area = r => 3.14 * r * r;
+            double areaRes = area(10);
+            Console.WriteLine("=================================");
+            Console.WriteLine("Func or Generic delegate recives and return something\nThe result of the Func delegate is: "+areaRes);
+            //Action
+            Console.WriteLine("=================================");
+            Action<string> printSomething = some => Console.WriteLine(some);
+            printSomething("Action delegate it doesnt return nothing, ir void\nAction delegate resul");
+            //predicate
+            Console.WriteLine("=================================\n Predicate delegate");
+            Predicate<int> checkAge = x => x >= 18;
+            Console.WriteLine(checkAge(18));
+            Console.ReadKey();
+        }
+    }
+}
